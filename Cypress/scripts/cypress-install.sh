c02da430f901bb71743ecf676d940412e5c12c98
TeamPapaya <34426841+edmundackah@users.noreply.github.com>
2024-02-28 13:28:16 +0000

    started working on multi cypress installations

--- Cypress/scripts/cypress-install.sh
+++ Cypress/scripts/cypress-install.sh
@@ -5,6 +5,14 @@ do
     esac
 done
 
+IFS=', ' read -r -a array <<< "$cypress_version"
+
+for element in "${array[@]}"
+do
+    echo "reading values: $element"
+done
+
+
 if [ ! -z "$cypress_version" ]; then
     echo "Installing Cypress $cypress_version"
 else
@@ -12,34 +20,36 @@ else
     echo "Cypress version not specified...\n Cypress $cypress_version will be installed"
 fi
 
-echo "Setting required environment variables"
+install_cypress() {
+    echo "Setting required environment variables"
 
-# Do not send crash reports to Cypress
-export CYPRESS_CRASH_REPORTS=0
+    # Do not send crash reports to Cypress
+    export CYPRESS_CRASH_REPORTS=0
 
-# Opt out of ads in CI logs
-export CYPRESS_COMMERCIAL_RECOMMENDATIONS=0
+    # Opt out of ads in CI logs
+    export CYPRESS_COMMERCIAL_RECOMMENDATIONS=0
 
-# Point Cypress to the root cache
-export CYPRESS_CACHE_FOLDER="/root/.cache/Cypress"
+    # Point Cypress to the root cache
+    export CYPRESS_CACHE_FOLDER="/root/.cache/Cypress"
 
-# Point projects to globally installed Cypress
-export NODE_PATH="/usr/local/lib/node_modules"
+    # Point projects to globally installed Cypress
+    export NODE_PATH="/usr/local/lib/node_modules"
 
-echo "Installing Cypress version $cypress_version"
+    echo "Installing Cypress version $cypress_version"
 
-wget --no-verbose -O /tmp/cypress.zip "https://cdn.cypress.io/desktop/$cypress_version/linux-x64/cypress.zip"
+    wget --no-verbose -O /tmp/cypress.zip "https://cdn.cypress.io/desktop/$cypress_version/linux-x64/cypress.zip"
 
-export CYPRESS_INSTALL_BINARY="/tmp/cypress.zip" 
+    export CYPRESS_INSTALL_BINARY="/tmp/cypress.zip" 
 
-npm install -g cypress@$cypress_version
+    npm install -g cypress@$cypress_version
 
-# Remove Cypress zip after installation
-rm /tmp/cypress.zip
-unset CYPRESS_INSTALL_BINARY
+    # Remove Cypress zip after installation
+    rm /tmp/cypress.zip
+    unset CYPRESS_INSTALL_BINARY
 
-# Prevent future Cypress installations from re-downloading binary
-export CYPRESS_INSTALL_BINARY=0
+    # Prevent future Cypress installations from re-downloading binary
+    export CYPRESS_INSTALL_BINARY=0
 
-echo "Verifying Cypress installation"
-npx cypress info
+    echo "Verifying Cypress installation"
+    npx cypress info
+}
